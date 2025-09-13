d0ed447a4de3b24164ed224b3201c7cf6e9e4b24
조흥륜 <heungryuncho@joheunglyun-ui-MacBookAir.local>
2025-09-13 22:50:21 +0900

    초기세팅

---
+++ src/main/java/com/example/fastbreak/FastbreakApplication.java
@@ -0,0 +1,13 @@
+package com.example.fastbreak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastbreakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastbreakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fastbreak/FastbreakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fastbreak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastbreakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
