ba918181ec1b50cfcb95973b8fecf9802b0b9392
heungryuncho <heungryuncho@naver.com>
2026-03-02 00:06:17 +0900

    chore: Spring Boot 3.5.11 및 Java 21 초기 프로젝트 환경 구성

---
+++ src/main/java/com/portfolio/fastbreak/FastbreakApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.fastbreak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastbreakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastbreakApplication.class, args);
+	}
+
+}
