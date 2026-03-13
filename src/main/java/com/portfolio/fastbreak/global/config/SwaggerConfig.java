c6217758ffc2d225d08bf4afdd6ba5cc50712e77
heungryuncho <heungryuncho@naver.com>
2026-03-13 23:31:00 +0900

    docs: Swagger API 적용

--- src/main/java/com/portfolio/fastbreak/global/config/SwaggerConfig.java
+++ src/main/java/com/portfolio/fastbreak/global/config/SwaggerConfig.java
@@ -1,4 +1,25 @@
 package com.portfolio.fastbreak.global.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Fastbreak API 문서 (MVP)")
+                .description("농구 티켓 예매 시스템(MVP)을 위한 프론트엔드 연동용 REST API 명세서입니다.")
+                .version("1.0.0");
+    }
 }
